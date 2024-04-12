eff3e1b336b1fcfd049599c98434a8fcb6d01018
Adei Cabañas <adeicabanas@MacBook-Air-de-Adei-2.local>
2024-04-11 20:17:29 -0600

    add dialog with angular material

--- src/app/layouts/dashboard/pages/students/students.component.ts
+++ src/app/layouts/dashboard/pages/students/students.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { IStudent } from './models';
+import { MatDialog } from '@angular/material/dialog';
+import { StudentDialogComponent } from './components/student-dialog/student-dialog.component';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
-  styleUrl: './students.component.scss'
+  styleUrl: './students.component.scss',
 })
 export class StudentsComponent {
+  displayedColumns: string[] = [
+    'id',
+    'firstName',
+    'lastName',
+    'email',
+    'createdAt',
+  ];
 
+  students: IStudent[] = [
+    {
+      id: 1,
+      firstName: 'Juan',
+      lastName: 'Lopez',
+      email: 'jLopez@mail.com',
+      createdAt: new Date(),
+    },
+    {
+      id: 2,
+      firstName: 'Michi',
+      lastName: 'Quimichi',
+      email: 'mQuimichi@mail.com',
+      createdAt: new Date(),
+    },
+    {
+      id: 3,
+      firstName: 'Luna',
+      lastName: 'Cabañas',
+      email: 'lCabañas@mail.com',
+      createdAt: new Date(),
+    },
+  ];
+
+  constructor(private matDialog: MatDialog) {}
+  openDialog(): void {
+    this.matDialog.open(StudentDialogComponent);
+  }
 }
--- src/app/layouts/dashboard/pages/students/students.module.ts
+++ src/app/layouts/dashboard/pages/students/students.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatTableModule } from '@angular/material/table';
 import { StudentsRoutingModule } from './students-routing.module';
 import { StudentsComponent } from './students.component';
-
+import { MatDialogModule } from '@angular/material/dialog';
+import { StudentDialogComponent } from './components/student-dialog/student-dialog.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [StudentsComponent],
-  imports: [CommonModule, StudentsRoutingModule],
+  declarations: [StudentsComponent, StudentDialogComponent],
+  imports: [
+    CommonModule,
+    StudentsRoutingModule,
+    MatTableModule,
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   exports: [StudentsComponent],
 })
 export class StudentsModule {}
