b69c880135971a899a5e4b47b54489b2ea785982
Adei Cabañas <adeicabanas@MacBook-Air-de-Adei-2.local>
2024-05-21 07:46:57 -0600

    update class

--- src/app/layouts/dashboard/pages/classes/components/classes-dialog/classes-dialog.component.ts
+++ src/app/layouts/dashboard/pages/classes/components/classes-dialog/classes-dialog.component.ts
@@ -1,18 +1,27 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CoursesService } from '../../../courses/courses.service';
+import { StudentService } from '../../../students/students.service';
+import { IStudent } from '../../../students/models';
+import { ICourses } from '../../../courses/models';
 import { IClass } from '../../models';
 
 @Component({
-  selector: 'app-courses-dialog',
+  selector: 'app-classes-dialog',
   templateUrl: './classes-dialog.component.html',
-  styleUrl: './classes-dialog.component.scss',
+  styleUrls: ['./classes-dialog.component.scss'],
 })
-export class ClassDialogComponent {
+export class ClassDialogComponent implements OnInit {
   classForm: FormGroup;
+  courses: ICourses[] = [];
+  students: IStudent[] = [];
+
   constructor(
     private formBuilder: FormBuilder,
     private matDialogRef: MatDialogRef<ClassDialogComponent>,
+    private coursesService: CoursesService,
+    private studentService: StudentService,
     @Inject(MAT_DIALOG_DATA) private editingCourse?: IClass
   ) {
     this.classForm = this.formBuilder.group({
@@ -20,8 +29,14 @@ export class ClassDialogComponent {
       studentId: ['', [Validators.required]],
       qty: ['', [Validators.required]],
     });
-    if (editingCourse) {
-      this.classForm.patchValue(editingCourse);
+  }
+
+  ngOnInit(): void {
+    this.loadCourses();
+    this.loadStudents();
+
+    if (this.editingCourse) {
+      this.classForm.patchValue(this.editingCourse);
     }
   }
 
@@ -43,5 +58,27 @@ export class ClassDialogComponent {
     } else {
       this.matDialogRef.close(this.classForm.value);
     }
+  }
+
+  loadStudents() {
+    this.studentService.getStudents().subscribe({
+      next: (value) => {
+        this.students = value;
+      },
+      error: (err) => {
+        console.error('Error loading students', err);
+      },
+    });
+  }
+
+  loadCourses() {
+    this.coursesService.getCourse().subscribe({
+      next: (value) => {
+        this.courses = value;
+      },
+      error: (err) => {
+        console.error('Error loading courses', err);
+      },
+    });
   }
 }
